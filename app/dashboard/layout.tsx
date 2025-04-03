4a619030c337d25d40de1bfd82f585a808843da4
Majid-Hassan <88468161+Majid-Hassan@users.noreply.github.com>
2025-04-03 23:13:16 +0500

    Add files via upload

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,16 @@
+import Sidebar from '@/components/dashboard/sidebar'
+import React from 'react'
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+
+    return (
+        <div className="min-h-screen w-full">
+            <div className="flex">
+                <Sidebar />
+                <div className="flex-1">{children}</div>
+            </div>
+        </div>
+    )
+}
+
+export default layout
